ab156c273777bd8589d8ad3e19a53672ce948ec6
DVilla2397 <a01337636@itesm.mx>
2019-11-21 18:06:28 -0600

    ASFA

--- Pizza.java
+++ Pizza.java
@@ -1,12 +1,21 @@
-import personajes.*;
+import javafx.secene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Pizza extends Heroes{
+public class Pizza extends HeredarHeroe{
 
 	public Pizza(){
-		super("Pizza",37,25,100,0);
+		super("Pizza",0,0,0,"Imagenes/Pizza.png");
+
+		Habilidad[] habilidades = new Habilidad[1];
+		habilidades[0] = new Habilidad("",0); 
 	}
-	public void atacar(Personajes p){
-		
+
+	public void atacar(Personajes enemigo){
+		System.out.println("la Pizza ataca");
+		enemigo.setHP(enemigo.getHP()-(getAtaque()-enemigo.getDefensa()));
 	}
+
+
+
 
 }
